1f3df898eb85ce9c96bb122ae49cdfac5f11adb8
antimonycodes <akinrinadetobilobasb@gmail.com>
2024-10-13 20:32:36 +0100

    "Added tooltips to QuickStat cards with custom styling"

--- src/components/Admin/Dashboard/QuickStat.tsx
+++ src/components/Admin/Dashboard/QuickStat.tsx
@@ -3,15 +3,26 @@ import assetIcon from "../../../assets/svgs/Dasseets.svg";
 // import scoreIcon from "../../../assets/svgs/DScore.svg";
 import scanIcon from "../../../assets/svgs/Dscan.svg";
 import infoIcon from "../../../assets/svgs/info.svg";
+// import { Tooltip } from "@mui/material";
+import UseTooltip from "../../../utils/useTooltip";
 
 const QuickStat = () => {
   const QuickStatCards = [
-    { title: "Total Risks", number: "216", percentage: "-10%", icon: riskIcon },
+    {
+      title: "Total Risks",
+      number: "216",
+      percentage: "-10%",
+      icon: riskIcon,
+      tooltipContent:
+        "Tooltips are used to describe or identify an element. In most scenarios, tooltips help the user understand meaning, function.",
+    },
     {
       title: "Total Assets",
       number: "3200",
       percentage: "+10%",
       icon: assetIcon,
+      tooltipContent:
+        "Tooltips are used to describe or identify an element. In most scenarios, tooltips help the user understand meaning, function.",
     },
     {
       title: "Safety Score",
@@ -19,37 +30,59 @@ const QuickStat = () => {
       percentage: "-40%",
       // icon: scoreIcon,
       icon: assetIcon,
+      tooltipContent:
+        "Tooltips are used to describe or identify an element. In most scenarios, tooltips help the user understand meaning, function.",
     },
-    { title: "Total Scans", number: "200", percentage: "+55%", icon: scanIcon },
+    {
+      title: "Total Scans",
+      number: "200",
+      percentage: "+55%",
+      icon: scanIcon,
+      tooltipContent:
+        "Tooltips are used to describe or identify an element. In most scenarios, tooltips help the user understand meaning, function.",
+    },
   ];
   return (
     <div className=" grid grid-cols-4 gap-4 ">
-      {QuickStatCards.map(({ title, icon, number, percentage }, index) => (
-        <div
-          key={index}
-          className=" rounded-lg shadow-[5px_5px_40px_rgba(107,151,255,0.3)] p-[20px] border flex flex-col gap-2 relative"
-        >
-          <div className=" flex items-center justify-between">
-            <img src={icon} alt="" width={40} height={40} />
-            <img src={infoIcon} className=" " width={20} alt="" />
+      {QuickStatCards.map(
+        ({ title, icon, number, percentage, tooltipContent }, index) => (
+          <div
+            key={index}
+            className=" rounded-lg shadow-[5px_5px_40px_rgba(107,151,255,0.3)] p-[20px] border flex flex-col gap-2 relative"
+          >
+            <div className=" flex items-center justify-between">
+              <img src={icon} alt="" width={40} height={40} />
+              {/* <img src={infoIcon} className=" " width={20} alt="" /> */}
+              {/* <Tooltip
+                title={tooltipContent}
+                arrow={{ offset: 40, color: "red", direction: "left" }}
+                placement="bottom-end"
+                classes={{ tooltip: "my-tooltip", arrow: "my-arrow" }}
+              >
+                <img src={infoIcon} className=" " width={20} alt="" />
+              </Tooltip> */}
+              <UseTooltip title={tooltipContent}>
+                <img src={infoIcon} className=" " width={20} alt="" />
+              </UseTooltip>
+            </div>
+            <h1 className=" text-[#667085] text-base font-medium leading-6 tracking-[0.5%]">
+              {title}
+            </h1>
+            <div className=" flex items-center gap-2">
+              <h1 className=" text-[#333843] font-medium">{number}</h1>
+              <span
+                className={`${
+                  percentage.includes("-")
+                    ? "text-[#F04438] bg-[#FEEDEC] "
+                    : "text-[#0D894F] bg-[#E7F4EE] "
+                } font-medium px-[6px] py-[2px]  rounded-full`}
+              >
+                {percentage}
+              </span>
+            </div>
           </div>
-          <h1 className=" text-[#667085] text-base font-medium leading-6 tracking-[0.5%]">
-            {title}
-          </h1>
-          <div className=" flex items-center gap-2">
-            <h1 className=" text-[#333843] font-medium">{number}</h1>
-            <span
-              className={`${
-                percentage.includes("-")
-                  ? "text-[#F04438] bg-[#FEEDEC] "
-                  : "text-[#0D894F] bg-[#E7F4EE] "
-              } font-medium px-[6px] py-[2px]  rounded-full`}
-            >
-              {percentage}
-            </span>
-          </div>
-        </div>
-      ))}
+        )
+      )}
     </div>
   );
 };
---
+++ src/utils/UseTooltip.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+const UseTooltip = ({ title, children }) => {
+  const [showTooltip, setShowTooltip] = useState(false);
+
+  const handleMouseEnter = () => {
+    setShowTooltip(true);
+  };
+
+  const handleMouseLeave = () => {
+    setShowTooltip(false);
+  };
+
+  return (
+    <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+      {children}
+      {showTooltip && (
+        <div className="absolute top-12 left-8 w-full bg-gray-200 text-gray-900 rounded-md p-2 shadow-md">
+          {title}
+          <div className="absolute top-[-20px] left-20 w-4 h-4 bg-gray-200 transform rotate-45"></div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default UseTooltip;
